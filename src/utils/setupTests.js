bf075dc8c7275197a82c971e2e5c9ab20706a22b
Joseph Tongay <dirk.tongay@gmail.com>
2018-12-04 17:36:56 -0600

    reserving jest config in setupTests file

--- src/utils/setupTests.js
+++ src/utils/setupTests.js
@@ -3,3 +3,39 @@
 import 'react-testing-library/cleanup-after-each';
 // this adds jest-dom's custom assertions
 import 'jest-dom/extend-expect';
+
+// "jest": {
+//   "moduleFileExtensions": [
+//     "ts",
+//     "tsx",
+//     "js",
+//     "json"
+//   ],
+//     "moduleNameMapper": {
+//     "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+//       "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js"
+//   },
+//   "transform": {
+//     "^.+\\.(ts|js)x?$": "ts-jest"
+//   },
+//   "setupTestFrameworkScriptFile": "<rootDir>/src/utils/setupTests.js",
+//     "collectCoverageFrom": [
+//       "src/**/*.{js,jsx,ts,tsx}",
+//       "!<rootDir>/node_modules/"
+//     ],
+//       "coverageThreshold": {
+//     "global": {
+//       "branches": 90,
+//         "functions": 90,
+//           "lines": 90,
+//             "statements": 90
+//     }
+//   },
+//   "testMatch": [
+//     "<rootDir>/src/**/*.spec.(ts|js|tsx)"
+//   ],
+//     "coverageReporters": [
+//       "text"
+//     ],
+//       "testEnvironment": "jsdom"
+// }
